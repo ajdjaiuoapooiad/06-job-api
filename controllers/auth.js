17de9b02d6256114919cffbc37ea36cc95772625
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-12-01 17:46:47 +0900

    ok

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const {StatusCodes} = require('http-status-codes')
 const User = require('../models/User')
+const {BadRequestError,UnauthenticatedError} = require('../errors')
 
 const register = async (req,res) => {
 
@@ -10,7 +11,17 @@ const register = async (req,res) => {
 
 
 const login = async (req,res) => {
-    res.send('Login')
+    const {email,password} = req.body
+
+    if(!email || !password){
+        throw new BadRequestError('Please provide email and password')
+    }
+    const user = await User.findOne({email})
+    if(!user){
+        throw new UnauthenticatedError('Invalid crediantial')
+    }
+    const token = user.createJWT()
+    res.status(StatusCodes.OK).json({user: {name: user.name},token})
 }
 
 
