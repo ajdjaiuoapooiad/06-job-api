ce2350bcea8fe86c8d76ffda81479f772127d65d
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-12-01 15:38:15 +0900

    ok

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,16 @@
 const {StatusCodes} = require('http-status-codes')
 const User = require('../models/User')
-
-
+const bcrypt = require('bcryptjs')
 
 const register = async (req,res) => {
-    const user = await User.create({...req.body})
+    const {name,email,password} = req.body
+
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(password,salt)
+
+    const tempUser = {name,email, password: hashPassword}
+
+    const user = await User.create({...tempUser})
     res.status(StatusCodes.CREATED).json({user})
 }
 
