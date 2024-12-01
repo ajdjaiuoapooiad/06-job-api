4343543cb507e0caf7e718a26497c2496d986eff
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-12-01 14:55:37 +0900

    175ch ok

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,11 @@
+const {StatusCodes} = require('http-status-codes')
+const User = require('../models/User')
+
+
+
 const register = async (req,res) => {
-    res.send('Register')
+    const user = await User.create({...req.body})
+    res.status(StatusCodes.CREATED).json({user})
 }
 
 
