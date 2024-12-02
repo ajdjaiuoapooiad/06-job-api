8a89dc73c21892d4a7cbc4449ae312b7f83401c8
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-12-02 19:37:04 +0900

    ok finish

--- app.js
+++ app.js
@@ -14,9 +14,27 @@ const connectDB = require('./db/connect')
 const authenticateUser = require('./middleware/authentication')
 
 
-app.use(express.json());
 // extra packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
 
+
+
+app.use(express.json())
+
+app.use(rateLimiter(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+))
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
+//router
 app.use('/api/v1/auth',authRouter)
 app.use('/api/v1/jobs',authenticateUser,jobRouter)
 
