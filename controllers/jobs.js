fa301966af4b64a9d4924c8e6022eaad197b701a
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-12-02 12:08:16 +0900

    189ch ok

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,12 +1,23 @@
+const Job = require("../models/Job")
+const {StatusCodes} = require('http-status-codes')
+
+
+
 const getAllJob = async (req,res) => {
     res.send('getAllJob')
 }
 const getJob = async (req,res) => {
     res.send('getJob')
 }
-const createJob = async (req,res) => {
-    res.json(req.user)
+
+
+const createJob = async (req, res) => {
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({ job })
 }
+
+
 const deleteJob = async (req,res) => {
     res.send('deleteJob')
 }
